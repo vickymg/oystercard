597909455db42324cb75e0679cbfb2b48a487257
Vicky Gray <vickygray26@hotmail.com>
2016-01-25 15:20:53 +0000

    added the balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,16 @@
-require 'Oystercard'
+require 'oystercard'
 
 describe Oystercard do
 
+  subject(:oystercard) {described_class.new}
+
+  describe "#initialize" do
+
+    it {is_expected.to respond_to(:balance)}
+
+    it "has an initial balance of 0" do
+      expect(oystercard.balance).to eq Oystercard::DEFAULT_BALANCE
+    end
+
+  end
 end
