91f41282868b1e74030a9270c70e2be47f6e077d
Sarah Crawley <sarah.crawley6@gmail.com>
2016-01-25 16:40:10 +0000

    deduct money from oyster card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,15 +3,19 @@ class Oystercard
 
   attr_reader :balance
 
-  MAXIMUM_LIMIT = 90
+  MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    raise "Cannot top up! Maximum limit #{MAXIMUM_LIMIT}!" if @balance + amount > MAXIMUM_LIMIT
+    raise "Cannot top up! Maximum limit #{MAXIMUM_BALANCE}!" if @balance + amount > MAXIMUM_BALANCE
   	@balance += amount
   end
+
+  def deduct(amount)
+  	@balance -= amount 
+  end 
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,18 +8,32 @@ describe Oystercard do
 		expect(oystercard.balance).to eq(0)
 	end
 
-	describe 'top_up' do
+	describe '#top_up' do
 
-   	it {is_expected.to respond_to(:top_up).with(1).argument}
+	   	it {is_expected.to respond_to(:top_up).with(1).argument}
 
-   	it 'should top up the balance' do
-   		oystercard.top_up(10)
-   		expect(oystercard.balance).to eq(10)
-         #above could be written using .to change .by
-   	end
+	   	it 'should top up the balance' do
+	   		oystercard.top_up(10)
+	   		expect(oystercard.balance).to eq(10)
+	         #above could be written using .to change .by
+	   	end
 
-		it "should have a maximum limit of #{Oystercard::MAXIMUM_LIMIT}" do
-			expect { oystercard.top_up(91) }.to raise_error "Cannot top up! Maximum limit #{Oystercard::MAXIMUM_LIMIT}!"
+		it 'should raise an error if maximum balance exceeded' do
+			maximum_balance = Oystercard::MAXIMUM_BALANCE
+			oystercard.top_up(maximum_balance)
+			expect { oystercard.top_up(1) }.to raise_error "Cannot top up! Maximum limit #{maximum_balance}!"
 		end
-	end
+	end 
+
+
+	describe '#deduct money' do
+
+		it {is_expected.to respond_to(:deduct).with(1).argument}
+
+		it 'should deduct the balance' do
+			oystercard.deduct(10)
+			expect(oystercard.balance).to eq(-10)
+		end 
+	end 
+
 end
