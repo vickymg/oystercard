6cee8a5e8e352545cce4a3be8affb3cd390bbaf1
itsindigo <matthewbridgesbusiness@gmail.com>
2016-01-26 12:19:36 +0000

    Added touchout deduct balance solution

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,13 +2,14 @@
 class Oystercard
 
   attr_reader :balance, :journey
+  attr_accessor :deduct
 
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
-    @journey = false 
+    @journey = false
   end
 
   def top_up(amount)
@@ -17,16 +18,16 @@ class Oystercard
   end
 
   def deduct(amount)
-  	@balance -= amount 
-  end 
+  	@balance -= amount
+  end
 
   def touch_in
   	raise "Insufficient funds" if @balance < MINIMUM_FARE
-  	@journey = true 
-  end 
+  	@journey = true
+  end
 
   def touch_out
   	@journey = false
+    @balance -= MINIMUM_FARE
   end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -23,44 +23,40 @@ describe Oystercard do
 			oystercard.top_up(maximum_balance)
 			expect { oystercard.top_up(1) }.to raise_error "Cannot top up! Maximum limit #{maximum_balance}!"
 		end
-	end 
+	end
 
 
 	describe '#deduct money' do
-
 		it {is_expected.to respond_to(:deduct).with(1).argument}
 
 		it 'should deduct the balance' do
 			oystercard.deduct(10)
 			expect(oystercard.balance).to eq(-10)
-			#above could be written using .to change .by
-		end 
-	end 
+			#NBabove could be written using .to change .by
+		end
+	end
 
 	describe 'journey' do
-
 		describe '#touch in' do
-
 			it 'should change the status of journey to true' do
 				oystercard.top_up(10)
 				expect {oystercard.touch_in}.to change {oystercard.journey}.from(false).to(true)
-			end  
+			end
 
 			it 'should raise an error if insufficient funds' do
 				expect {oystercard.touch_in}.to raise_error "Insufficient funds"
-			end 
-
-		end 
+			end
+		end
 
 		describe '#touch out' do
-
 			it 'should change the status of journey to false' do
 				oystercard.top_up(10)
 				oystercard.touch_in
 				expect {oystercard.touch_out}.to change {oystercard.journey}.from(true).to(false)
 			end
-
-		end 
+			it 'should deduct fare at end of journey (touch_out)' do
+				expect {oystercard.touch_out}.to change {oystercard.balance}.by(oystercard.deduct(1))
+			end
+		end
 	end
-
 end
