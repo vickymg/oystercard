dc91446260712542cbec9812cfccba1e3af1722c
itsindigo <matthewbridgesbusiness@gmail.com>
2016-01-26 17:32:01 +0000

    chp12

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,16 @@
 
 class Oystercard
 
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :journey_history, :record_journey, :entry_station, :exit_station
 
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
+  # DEFAULT_OYSTER_HISTORY = Array.new
 
   def initialize
     @balance = 0
+    @record_journey = {}
+    @journey_history = []
   end
 
   def top_up(amount)
@@ -15,25 +18,26 @@ class Oystercard
   	@balance += amount
   end
 
-  def touch_in(station)
+  def touch_in(entry_station)
   	raise "Insufficient funds" if @balance < MINIMUM_FARE
-  	@entry_station = station
-
+    @entry_station = entry_station
+  	@record_journey[:entry] = entry_station
   end
 
-  def touch_out
+  def touch_out(exit_station)
     deduct(MINIMUM_FARE)
+    @exit_station = exit_station
     @entry_station = nil
+    @record_journey[:exit] = exit_station
   end
 
   def in_journey?
-  	!!entry_station 
-  end 
+  	!!entry_station
+  end
 
   private
 
   def deduct(amount)
   	@balance -= amount
   end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -24,25 +24,26 @@ describe Oystercard do
 
 	describe 'journey' do
 
-		let(:station) {double :station}
+		let(:entry_station) {double :station}
+		let(:exit_station) {double :station}
 
 		describe '#touch in' do
 
 			it 'should change the status of journey to true' do
 				oystercard.top_up(10)
-        oystercard.touch_in(station)
-				expect(oystercard.in_journey?).to eq true 
+        oystercard.touch_in(entry_station)
+				expect(oystercard.in_journey?).to eq true
 			end
 
 			it 'should raise an error if insufficient funds' do
-				expect {oystercard.touch_in(station)}.to raise_error "Insufficient funds"
+				expect {oystercard.touch_in(entry_station)}.to raise_error "Insufficient funds"
 			end
 
 			it 'should remember the name of the entry station' do
 				oystercard.top_up(10)
-				oystercard.touch_in(station)
-				expect(oystercard.entry_station).to eq station
-			end 
+				oystercard.touch_in(entry_station)
+				expect(oystercard.entry_station).to eq entry_station
+			end
 
 		end
 
@@ -50,19 +51,41 @@ describe Oystercard do
 
 			it 'should change the status of journey to false' do
 				oystercard.top_up(10)
-				oystercard.touch_in(station)
-				expect {oystercard.touch_out}.to change {oystercard.in_journey?}.from(true).to(false)
+				oystercard.touch_in(entry_station)
+				expect {oystercard.touch_out(exit_station)}.to change {oystercard.in_journey?}.from(true).to(false)
 			end
 
 			it 'should deduct fare at end of journey (touch_out)' do
-				expect {oystercard.touch_out}.to change {oystercard.balance}.by(-Oystercard::MINIMUM_FARE)
+				expect {oystercard.touch_out(exit_station)}.to change {oystercard.balance}.by(-Oystercard::MINIMUM_FARE)
+			end
+
+			it 'should remember the name of the exit station' do
+				oystercard.top_up(10)
+				oystercard.touch_in(entry_station)
+				oystercard.touch_out(exit_station)
+				expect(oystercard.exit_station).to eq exit_station
 			end
 
 			it 'should should nillify the entry station' do
 				oystercard.top_up(10)
-				oystercard.touch_in(station)
-				expect {oystercard.touch_out}.to change {oystercard.entry_station}.to eq nil
-			end 
+				oystercard.touch_in(entry_station)
+				expect {oystercard.touch_out(exit_station)}.to change {oystercard.entry_station}.to eq nil
+			end
 		end
+
+		describe '#journey_creation' do
+			it 'holds a journey log' do
+			  expect(oystercard.journey_history).to be_empty
+			end
+
+			it 'stores a journey' do
+				oystercard.top_up(10)
+				oystercard.touch_in(entry_station)
+				oystercard.touch_out(exit_station)
+				expect(oystercard.journey_history).to include record_journey
+			end
+
+		end
+
 	end
 end
